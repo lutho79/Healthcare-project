0554260e9a391f494b9ca108219265ebc8f5e1e3
lutho79 <130660265+lutho79@users.noreply.github.com>
2023-07-03 15:14:24 +0200

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class calculator implements Actionlistener {
+
+
+  
+  public static void main(string[] args) {
+    // TODO Auto-generated method stub
